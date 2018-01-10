05db713db384fba85320786d5eb9e0591c801a18
stephecloutier <stephecloutier@gmail.com>
2018-01-10 14:31:33 +0100

    Added v1 to api url

--- config/settings/urls.py
+++ config/settings/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Jiri.main.urls')),
-    path('api/', include('Jiri.api.urls')),
+    path('api/v1/', include('Jiri.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
