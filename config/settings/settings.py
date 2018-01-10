f9799036795f11d445ab8058253b759575f2205c
stephecloutier <stephecloutier@gmail.com>
2018-01-10 14:24:11 +0100

    Corrected template path

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Jiri.templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../Jiri/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
