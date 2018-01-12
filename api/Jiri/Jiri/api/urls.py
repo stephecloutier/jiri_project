8272e8944a8e60fdcb0f9f00a9410c36aac353e8
stephecloutier <stephecloutier@gmail.com>
2018-01-12 18:28:35 +0100

    Named url patterns

--- api/Jiri/Jiri/api/urls.py
+++ api/Jiri/Jiri/api/urls.py
@@ -1,20 +1,18 @@
 from django.urls import path
 from django.conf.urls import include
 # from rest_framework import routers
-# from Jiri.api import views
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from Jiri.api import views
 
-urlpatterns = [
-    path('', views.api_root)
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>', views.UserDetail.as_view()),
-    path('events/', views.EventList.as_view()),
-    path('events/<int:pk>', views.EventDetail.as_view()),
-]
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('events/', views.EventList.as_view(), name='event-list'),
+    path('events/<int:pk>', views.EventDetail.as_view() name='event-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
