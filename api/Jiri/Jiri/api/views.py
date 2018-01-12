7f5624eed5d0c99559aeb01b38255422565a5d98
stephecloutier <stephecloutier@gmail.com>
2018-01-12 16:48:41 +0100

    Used generic class-based views to have even more concise code

--- api/Jiri/Jiri/api/views.py
+++ api/Jiri/Jiri/api/views.py
@@ -1,54 +1,25 @@
-# from django.shortcuts import render
 # from rest_framework import viewsets
-
-# from django.http import Http404
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
 
 from .models import User
 from .serializers import UserSerializer
-
-from rest_framework import mixins
 from rest_framework import generics
 
 ## DJR Tutorial
 
-
-class UserList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class UserList(generics.ListCreateAPIView):
     """
     List all users, or create a new user
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class UserDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a user
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
 
 
 ########## old ###########
