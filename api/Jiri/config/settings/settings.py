97105416281396e3418ce87f05a140879cdfb69b
stephecloutier <stephecloutier@gmail.com>
2018-01-12 18:29:58 +0100

    Added pagination

--- api/Jiri/config/settings/settings.py
+++ api/Jiri/config/settings/settings.py
@@ -112,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Rest Framework
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     # 'DEFAULT_PERMISSION_CLASSES': [
