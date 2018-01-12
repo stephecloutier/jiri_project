58d1dcd80f1197f40a6ccd5405e8b933c54b3e50
stephecloutier <stephecloutier@gmail.com>
2018-01-12 12:33:35 +0100

    Added new models

--- api/Jiri/Jiri/api/models.py
+++ api/Jiri/Jiri/api/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.EmailField(unique=True)
-    profile_pic = models.FileField(upload_to='uploads/users/', default='default-pic.png')
+    profile_pic = models.ImageField(upload_to='uploads/users/', default='default-pic.png')
     is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -23,10 +23,48 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    profile_pic = models.FileField(upload_to='uploads/students/', default='default-pic.png')
+    profile_pic = models.ImageField(upload_to='uploads/students/', default='default-pic.png')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(default=None, blank=True, null=True)
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    default_weight = models.DecimalField(max_digits=3, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=None, blank=True, null=True)
+
+class Event(models.Model):
+    JANVIER = 'JAN'
+    JUIN = 'JUN'
+    SEPTEMBRE = 'SEP'
+    SESSION_IN_YEAR_CHOICES = (
+        (JANVIER, 'Janvier'),
+        (JUIN, 'Juin'),
+        (SEPTEMBRE, 'Septembre'),
+    )
+    course_name = models.CharField(max_lenght=50)
+    exam_session = models.CharField(max_length=3, choices=SESSION_IN_YEAR_CHOICES)
+    date = models.DateField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=None, blank=True, null=True)
+
+class Implementation(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCASDE)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+    url_project = models.URLField(blank=True, null=True)
+    url_repo = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=None, blank=True, null=True)
+    def __str__(self):
+        return self.student + ' ' + self.project.name + ' ' + self.event
