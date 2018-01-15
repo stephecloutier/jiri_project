c2f3abf46d97d2a9bcba6d8fbfc1c19a5db9f20d
stephecloutier <stephecloutier@gmail.com>
2018-01-15 18:47:26 +0100

    Override destroy method with my soft delete

--- api/Jiri/Jiri/api/views.py
+++ api/Jiri/Jiri/api/views.py
@@ -1,6 +1,5 @@
-from .models import User, Event, Student
-from .serializers import UserSerializer, EventSerializer, StudentSerializer
-from .permissions import IsOwnerOrReadOnly, IsAdmin, IsAdminOrReadOnly
+from pprint import pprint
+import datetime
 
 from rest_framework import generics
 from rest_framework import permissions
@@ -8,6 +7,11 @@ from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import viewsets
+from rest_framework import status
+
+from .models import User, Event, Student, Project
+from .serializers import UserSerializer, EventSerializer, StudentSerializer, ProjectSerializer
+from .permissions import IsOwnerOrReadOnly, IsAdmin, IsAdminOrReadOnly
 
 ## DJR Tutorial
 @api_view(['GET'])
@@ -25,6 +29,11 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, IsAdmin)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    def destroy(self, request, pk):
+        user = User.objects.get(id=pk)
+        user.deleted_at = datetime.datetime.now()
+        user.save()
+        return Response(request.data, status=status.HTTP_204_NO_CONTENT)
 
 class StudentViewSet(viewsets.ModelViewSet):
     """
@@ -33,6 +42,17 @@ class StudentViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, IsAdminOrReadOnly)
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    def destroy(self, request, pk):
+        student = Student.objects.get(id=pk)
+        student.deleted_at = datetime.datetime.now()
+        student.save()
+        return Response(request.data, status=status.HTTP_204_NO_CONTENT)
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides 'list', 'create', 'retrieve', 'update' and 'destroy' actions.
+    """
+    pass
 
 class EventViewSet(viewsets.ModelViewSet):
     """
@@ -44,6 +64,12 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def destroy(self, request, pk):
+        event = Event.objects.get(id=pk)
+        event.deleted_at = datetime.datetime.now()
+        event.save()
+        return Response(request.data, status=status.HTTP_204_NO_CONTENT)
 
 
 ## old readonly for users
