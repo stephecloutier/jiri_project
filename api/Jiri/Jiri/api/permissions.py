c5121a05f5322d8bace37ea17919741c206293a7
stephecloutier <stephecloutier@gmail.com>
2018-01-15 17:31:18 +0100

    Fixed permissions to be global

--- api/Jiri/Jiri/api/permissions.py
+++ api/Jiri/Jiri/api/permissions.py
@@ -16,21 +16,21 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     """
-    Custom permission to only allow admin users to access and edit an object.
+    Custom permission to only allow admin users to access and edit a resource.
     """
 
     message = 'Vous devez être administrateur pour accéder à cette vue.'
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return request.user.is_admin
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow admin users to edit an object.
+    Custom permission to only allow admin users to edit a resource.
     """
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
+        
         return request.user.is_admin
