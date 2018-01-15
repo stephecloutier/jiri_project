204ad1f06ba91f919162e1eef29af10b11966dbd
stephecloutier <stephecloutier@gmail.com>
2018-01-15 15:55:42 +0100

    Added password hashing to create method in UserSerializer

--- api/Jiri/Jiri/api/serializers.py
+++ api/Jiri/Jiri/api/serializers.py
@@ -1,16 +1,26 @@
 from .models import User, Event
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from django.http import HttpResponse
+
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Event
-        fields = ('url', 'id', 'course_name', 'exam_session', 'exam_date', 'user')
+        fields = ('id', 'course_name', 'exam_session', 'exam_date', 'user')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     events = serializers.HyperlinkedRelatedField(many=True, view_name='event-detail', read_only=True)
+
+    def create(self, validated_data):
+        password = make_password(validated_data.get('password', None))
+        validated_data.pop("password")
+        return User.objects.create(password=password, **validated_data)
+
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'first_name', 'last_name', 'profile_pic', 'is_admin', 'events')
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'profile_pic', 'is_admin', 'events')
