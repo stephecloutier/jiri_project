e9388d840ce24ec1831199f16bdde25f57c151be
stephecloutier <stephecloutier@gmail.com>
2018-01-15 15:56:05 +0100

    Added custom permission to verify if a user is admin

--- api/Jiri/Jiri/api/permissions.py
+++ api/Jiri/Jiri/api/permissions.py
@@ -2,14 +2,24 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow users of an object to edit it.
+    Custom permission to only allow owner of an object to edit it.
     """
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
+        # so we'll always allow GET, HEAD or OPTIONS requests.  
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the user (owner) of the object
         return obj.user == request.user
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Custom permission to only allow admin users to access and edit an object.
+    """
+    message = 'Vous devez être administrateur pour accèder à cette vue.'
+
+    def has_object_permission(self, request, view, obj):
+        print(request.user)
+        return request.user.is_admin
