3c12d97637dd77c725b930dde5bc7e5f5108e015
stephecloutier <stephecloutier@gmail.com>
2018-01-16 14:33:58 +0100

    Modified update methods on serializers

--- api/Jiri/Jiri/api/serializers.py
+++ api/Jiri/Jiri/api/serializers.py
@@ -13,14 +13,18 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         validated_data.pop("password")
         return User.objects.create(password=password, **validated_data)
     def update(self, instance, validated_data):
-        pprint(validated_data.get('password', None))
-        if validated_data.get('password', None) is None:
-            return instance
-        instance.password = make_password(validated_data.get('password', None))
-        instance.save()
+        if validated_data.get('password', None) is not None:
+            instance.password = make_password(validated_data.get('password', None))
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.profile_pic = validated_data.get('profile_pic', instance.profile_pic)
+        if self.context['request'].user.is_admin:
+            instance.is_admin = validated_data.get('is_admin', instance.is_admin)
+        else:
+            raise serializers.ValidationError('Vous devez être administrateur pour modifier votre statue admin.')
         return instance
         
-
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'profile_pic', 'is_admin', 'meetings', 'deleted_at')
@@ -70,11 +74,10 @@ class ScoreSerializer(serializers.HyperlinkedModelSerializer):
             raise serializers.ValidationError('Le score doit être compris entre 0 et 20 inclus.')
     def update(self, instance, validated_data):
         instance.score = validated_data.get('score', instance.score)
-        if instance.score <= 20 and instance.score >= 0:
-            instance.save()
-            return instance
-        else:
-            raise serializers.ValidationError('Le score doit être compris entre 0 et 20 inclus.')
+        if instance.score > 20 or instance.score < 0:
+            raise serializers.ValidationError('Le score doit être compris entre 0 et 20 inclus.')      
+        instance.comment = validated_data.get('comment', instance.comment)
+        return instance
     class Meta:
         model = Score
         fields = ('id', 'score', 'comment', 'meeting', 'implementation', 'deleted_at')
@@ -91,11 +94,11 @@ class PerformanceSerializer(serializers.HyperlinkedModelSerializer):
             raise serializers.ValidationError("Le score final doit être compris entre 0 et 20 inclus.")
     def update(self, instance, validated_data):
         instance.final_score = validated_data.get('final_score', instance.final_score)
-        if instance.final_score <= 20 and instance.final_score >=0:
-            instance.save()
-            return instance
-        else:
+        if instance.final_score > 20 and instance.final_score < 0:
             raise serializers.ValidationError('Le score doit être compris entre 0 et 20 inclus.')
+        
+        return instance
+            
     class Meta:
         model = Performance
         fields = ('id', 'final_score', 'student', 'event', 'deleted_at')
